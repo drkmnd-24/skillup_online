7aaf3572e0d20d01b6e98cf51d0a5b1d5813bc5c
Vanj Mampusti <vanjo.mampusti0324@gmail.com>
2025-08-31 19:05:39 +0800

    import libraries

--- skillup_app/views.py
+++ skillup_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.db import transaction
+
+from rest_framework import generics, viewsets, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FormParser
+
+from .models import (UploadMarkdownFile, ModifiedMarkdownFile, TaskTemplate, Assignment)
+from .serializers import (RegistrationSerializer, UserSerializer, UploadMarkdownFile, ModifiedMarkdownFile,
+                          TaskTemplateSerializer, AssignmentSerializer, MyAssignmentSerializer)
+from .permissions import IsAssignee
 
 # Create your views here.
